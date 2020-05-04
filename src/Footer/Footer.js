14f9fcfa89d84142428b2aa60a24f3fdcfbf22d4
iliyand <iliqn1994@gmail.com>
2020-05-04 22:23:28 +0100

    small change

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -9,7 +9,7 @@ function MainSection() {
             <footer>
             <div className="row">
                 <div className="col-md-6">
-                    <p>Copyright &copy; 2020 Coffee&Code</p>
+                    <p>Copyright &copy; 2020 ID</p>
                 </div>
                 <div className="col-md-6 text-md-right">
                     <a href="foo" className="text-dark">Terms of Use</a>
