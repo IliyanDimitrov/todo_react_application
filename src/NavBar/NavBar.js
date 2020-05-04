aad09889c7f4ddd880f880a27938efc8ab1748f9
iliyand <iliqn1994@gmail.com>
2020-05-04 18:14:00 +0100

    nav bar enchantments

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -7,7 +7,7 @@ function NavBar(props) {
         <header>
             <nav class="navbar navbar-expand-md navbar-light bg-light mb-3">
                 <div class="container-fluid">
-                    <a href="foo" class="navbar-brand mr-3">
+                    <a href="#top" class="navbar-brand mr-3">
                         <img src="" width="30" height="30"
                             class="d-inline-block align-top" alt="" />
                     Todo Application
@@ -15,15 +15,14 @@ function NavBar(props) {
                     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-
                     <div class="collapse navbar-collapse" id="navbarCollapse">
                         <div class="navbar-nav">
-                            <a href="foo" class="nav-item nav-link active">Pending</a>
-                            <a href="foo" class="nav-item nav-link">Completed</a>
+                            <a href="#top" class="nav-item nav-link active">Pending</a>
+                            <a href="#top" class="nav-item nav-link">Completed</a>
                         </div>
                         <div class="navbar-nav ml-auto">
-                            <a href="foo" class="nav-item nav-link">Register</a>
-                            <a href="foo" class="nav-item nav-link">Login</a>
+                            <a href="#top" class="nav-item nav-link">Register</a>
+                            <a href="#top" class="nav-item nav-link">Login</a>
                         </div>
                     </div>
                 </div>
